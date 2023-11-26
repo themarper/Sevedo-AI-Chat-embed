1b7b4121e39c9a928aa9e933127d3afe89ae7595
themarper <themarper@gmail.com>
2023-11-27 02:13:19 +0700

    Remove Branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -61,7 +61,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Sevedo</span>
+        <span> Sevedo </span>
       </a>
     </span>
   )
