e21485bd31639b25d12eea5ff9541d16d1aa0841
themarper <themarper@gmail.com>
2023-11-27 01:32:48 +0700

    Remove Branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,14 +54,14 @@ export const Badge = (props: Props) => {
     }}>Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://sevedo.it'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Sevedo</span>
       </a>
     </span>
   )
