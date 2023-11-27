26df593b030fea2de43070ce9826a9e501e5cc18
themarper <themarper@gmail.com>
2023-11-27 13:53:17 +0700

    Remove Branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,7 +54,7 @@ export const Badge = (props: Props) => {
     }}>Powered by
       <a
         ref={liteBadge}
-        href={'https://sevedo.it'}
+        href={'https://sevedo.it/'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
